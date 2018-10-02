333e719fc12630dcd979c78df36723434533114d
David Blümel <bludac16@htl-kaindorf.ac.at>
2018-10-02 09:21:42 +0200

    Finished class: Appointment

--- Exercise_103_Terminkalender/src/Appointment.java
+++ Exercise_103_Terminkalender/src/Appointment.java
@@ -1,3 +1,7 @@
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,5 +13,35 @@
  * @author David
  */
 public class Appointment {
+    private String activity;
+    private LocalDateTime date;
+    private DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd.MM.yyyy - HH.mm");
+
+    public Appointment(String activity, LocalDateTime date) {
+        this.activity = activity;
+        this.date = date;
+    }
+
+    public String getActivity() {
+        return activity;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setActivity(String activity) {
+        this.activity = activity;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s --> %s", date.format(dtf), activity);
+    }
+     
     
 }
