45bbccbe36f0041b8ed4c6a875de2ba8b88dccee
David Blümel <bludac16@htl-kaindorf.ac.at>
2018-10-02 09:45:54 +0200

    Worked on class: AppointmentModell

--- Exercise_103_Terminkalender/src/AppointmentModell.java
+++ Exercise_103_Terminkalender/src/AppointmentModell.java
@@ -1,3 +1,14 @@
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import javax.swing.AbstractListModel;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -8,6 +19,48 @@
  *
  * @author David
  */
-public class AppointmentModell {
+public class AppointmentModell extends AbstractListModel{
+
+    private ArrayList<Appointment> appointments = new ArrayList<>();
+    
+    public void add(Appointment a)
+    {
+        appointments.add(a);
+        fireIntervalAdded(this, appointments.size()-1, appointments.size()-1);
+    }
+    
+    public void save(File f) throws FileNotFoundException, IOException
+    {
+        ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(f));
+        for (Appointment a: appointments) {
+            oos.writeObject(a);
+        }
+        oos.flush();
+        oos.close();
+    }
+    public void load(File f) throws FileNotFoundException, IOException, ClassNotFoundException
+    {
+        ObjectInputStream ois = new ObjectInputStream(new FileInputStream(f));
+        try{
+            Object o;
+            while ((o = ois.readObject()) != null)
+            {
+                appointments.add((Appointment) o);
+            }
+        } catch (Exception Ex)
+        {
+            
+        }
+    }
+    
+    @Override
+    public int getSize() {
+        return appointments.size();
+    }
+
+    @Override
+    public Object getElementAt(int i) {
+        return appointments.get(i);
+    }
     
 }
